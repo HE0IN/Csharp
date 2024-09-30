7975850ab80cda334a740c787f14f68bee033d30
HE0IN <duddls8029@gmail.com>
2024-09-30 09:18:23 +0900

    Examples 파일 추가

---
+++ Examples/Program.cs
@@ -0,0 +1,14 @@
+﻿// 네임스페이스 : 클래스이름의 중복을 제거한다.
+namespace Example
+{
+    // internal class program
+    internal class Program
+    {
+        // static void main() : 프로그램 시작점 (Entrt Point)
+        static void Main(string[] args)
+        {
+            // Console.writeLine : 콘솔창에 문자열 출력 함수
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
